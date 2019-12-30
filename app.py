15d7f81d72d111f8581e3203d0af9888cbf0d317
Mantongash <you@example.com>
2019-12-30 14:58:23 +0300

    Appended the image list with the image url from the articles

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
+
 @app.route("/")
-  def index():
-    newsapi = NewsApiClient(api_key = "519b09bcd038461aa28d5cf702b9e0e9")
-    topheadlines = newsapi.get_top_headlines(source = "al-jazeera-english")
+def index():
+        newsapi = NewsApiClient(api_key="519b09bcd038461aa28d5cf702b9e0e9")
+        topheadlines = newsapi.get_top_headlines(source="al-jazeera-english")
 
-    articles = topheadlines ["articles"]
+        articles = topheadlines["articles"]
 
-    desc = []
-    news = []
-    img = []
+        desc = []
+        news = []
+        img = []
 
-    for i in range(len(articles)):
-      myarticles = articles[i]
+        for i in range(len(articles)):
+            myarticles = articles[i]
 
-      news.append(myarticles["title"])
-      desc.append(myarticles["description"])
+            news.append(myarticles["title"])
+            desc.append(myarticles["description"])
+            img.append(myarticles["urlToImage"])
