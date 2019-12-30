635a0d4c379bfaa1bf36863fbce41510975cef8a
Mantongash <you@example.com>
2019-12-30 14:43:45 +0300

    Created an articles object

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 @app.route("/")
   def index():
     newsapi = NewsApiClient(api_key = "519b09bcd038461aa28d5cf702b9e0e9")
-    topheadlines = newsapi.get_top_headlines(source = "al-jazeera-english
-")
+    topheadlines = newsapi.get_top_headlines(source = "al-jazeera-english")
+
+    articles = topheadlines ["articles"]
