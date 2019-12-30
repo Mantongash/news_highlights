778c65d3f9a8339f5667ae8b5ec8e4eeb16cd193
Mantongash <you@example.com>
2019-12-30 14:42:13 +0300

    Created a topheadlines object and linked it to a source

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ app = Flask(__name__)
 @app.route("/")
   def index():
     newsapi = NewsApiClient(api_key = "519b09bcd038461aa28d5cf702b9e0e9")
+    topheadlines = newsapi.get_top_headlines(source = "al-jazeera-english
+")
