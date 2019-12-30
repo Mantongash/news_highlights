328fcc9e8dde594ea7736d72c451b63aefae6093
Mantongash <you@example.com>
2019-12-30 15:11:31 +0300

    Added code to run the app

--- app.py
+++ app.py
@@ -6,22 +6,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-        newsapi = NewsApiClient(api_key="519b09bcd038461aa28d5cf702b9e0e9")
-        topheadlines = newsapi.get_top_headlines(source="al-jazeera-english")
+    newsapi = NewsApiClient(api_key="519b09bcd038461aa28d5cf702b9e0e9")
+    topheadlines = newsapi.get_top_headlines(source="al-jazeera-english")
 
-        articles = topheadlines["articles"]
+    articles = topheadlines["articles"]
 
-        desc = []
-        news = []
-        img = []
+    desc = []
+    news = []
+    img = []
 
-        for i in range(len(articles)):
-            myarticles = articles[i]
+    for i in range(len(articles)):
+        myarticles = articles[i]
 
-            news.append(myarticles["title"])
-            desc.append(myarticles["description"])
-            img.append(myarticles["urlToImage"])
+        news.append(myarticles["title"])
+        desc.append(myarticles["description"])
+        img.append(myarticles["urlToImage"])
 
-        mylist = zip(news, desc, img)
+    mylist = zip(news, desc, img)
 
-        return render_template("index.html", context = mylist)
+    return render_template("index.html", context=mylist)
+
+
+if __name__ == " __main__":
+    app.run(debug=True)
