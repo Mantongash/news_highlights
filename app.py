74dc877c13b8e4d302ff2f6a2054739c708799b3
Mantongash <you@example.com>
2020-01-06 12:23:13 +0300

    Added home.html in the templates

--- app.py
+++ app.py
@@ -27,6 +27,29 @@ def index():
     return render_template("index.html", context=mylist)
 
 
+@app.route("/bbc")
+def bbc():
+    newsapi = NewsApiClient(api_key="519b09bcd038461aa28d5cf702b9e0e9")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+
+    articles = topheadlines["articles"]
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles["title"])
+        desc.append(myarticles["description"])
+        img.append(myarticles["urlToImage"])
+
+    mylist = zip(news, desc, img)
+
+    return render_template("bbc.html", context=mylist)
+
+
 if __name__ == " __main__":
     app.debug = True
     app.run()
