7c6cfcdd918ba82ae82bc6798984a43c235d9cb5
Mantongash <you@example.com>
2020-01-07 09:53:42 +0300

    Added functionality to routes

--- app.py
+++ app.py
@@ -59,6 +59,60 @@ def bbc():
     return render_template("bbc.html", context=mylist)
 
 
+@app.route("/abc")
+def abc():
+    newsapi = NewsApiClient(api_key="519b09bcd038461aa28d5cf702b9e0e9")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
+
+    articles = topheadlines["articles"]
+
+    desc = []
+    news = []
+    img = []
+    url = []
+    pubAt = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles["title"])
+        desc.append(myarticles["description"])
+        img.append(myarticles["urlToImage"])
+        url.append(myarticles["url"])
+        pubAt.append(myarticles["publishedAt"])
+
+    mylist = zip(news, desc, img, url, pubAt)
+
+    return render_template("abc.html", context=mylist)
+
+
+@app.route("/cnn")
+def cnn():
+    newsapi = NewsApiClient(api_key="519b09bcd038461aa28d5cf702b9e0e9")
+    topheadlines = newsapi.get_top_headlines(sources="associated-press")
+
+    articles = topheadlines["articles"]
+
+    desc = []
+    news = []
+    img = []
+    url = []
+    pubAt = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles["title"])
+        desc.append(myarticles["description"])
+        img.append(myarticles["urlToImage"])
+        url.append(myarticles["url"])
+        pubAt.append(myarticles["publishedAt"])
+
+    mylist = zip(news, desc, img, url, pubAt)
+
+    return render_template("cnn.html", context=mylist)
+
+
 if __name__ == " __main__":
     app.debug = True
-    app.run()
+    app.run
